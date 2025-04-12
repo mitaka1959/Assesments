4a43f327c9d362d07adf9988060b1e7031d105e3
mitaka1959 <126474807+mitaka1959@users.noreply.github.com>
2025-04-12 07:48:54 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,134 +1,156 @@
 ﻿using System;
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Xml.Linq;
+using System.Security;
+using System.Reflection.Metadata;
 
-abstract class Vehicle
+public delegate bool Condition(int number);
+public delegate bool ConditionCount(int number, List<int> numbers1);
+public delegate float CalculateDDS(float sum);
+
+public class Actor
 {
-    public  string Make { get; set; }
+    public string firstName { get; set; }
 
-    public  string Model { get; set; }
-
-    public int Year { get; set; }
-
-    public Vehicle(string make, string model, int year) 
-    {
-        Make = make;
-        Model = model;
-        Year = year;
-    }
-
-    public abstract void StartTheEngine();
-
-    public override string ToString()
-    {
-        return $" {Make} {Model}";
-    }
+    public string lastName { get; set; }
 }
 
-class Car : Vehicle
+public class Movie 
 {
-    public int Doors { get; set; }
+    public string title { get; set; }
 
-    public Car(string make, string model,int year, int doors) : base(make, model, year)
-    {
-        Doors = doors;
-        
-    }
-    public override void StartTheEngine()
-    {
-        Console.WriteLine("The engine starts with a press of a button");
-    }
+    public string starring { get; set; }
 
-    public Car Clone()
-    {
-        return new Car(Make, Model, Year, Doors);
-    }
-
-    public override string ToString()
-    {
-        return $"{base.ToString()}, Doors: {Doors} ";
-    }
+    public double profits { get; set; }
 }
-
-class Plane : Vehicle
-{
-    public int Wingspan { get; set; }
-
-    public Plane(int wingspan, string make, string model, int year) : base(make, model, year)
-    {
-        Wingspan = wingspan;
-
-
-    }
-    public override void StartTheEngine()
-    {
-        Console.WriteLine("I have no clue how to start the engine");
-    }
-
-    public Plane Clone()
-    {
-        return new Plane(Wingspan, Make, Model, Year);
-    }
-
-    public override string ToString()
-    {
-        return $"{base.ToString()}, Wingspan: {Wingspan} meters";
-    }
-
-}
-
-class Garage : IEnumerable<Vehicle>
-{
-    private List<Vehicle> vehicles;
-
-    public Garage()
-    {
-        vehicles = new List<Vehicle>();
-    }
-
-    public void AddVehicle(Vehicle vehicle) 
-    { 
-        vehicles.Add(vehicle);
-    }
-    public IEnumerator<Vehicle> GetEnumerator()
-    {
-        foreach (var vehicle in vehicles)
-        {
-            yield return vehicle;
-        }
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-}
-
-
 class Program
 {
-    static void Main()
+    public static void Main(string[] args)
     {
-        Garage garage = new Garage();
+        List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<int> numbers1 = new List<int> { 1, 2, 2, 2, 4, 5, 5, 6, 7, 7, 8, 9 };
+        List<float> sums = new List<float> { 1300f, 2150f, 4000f, 5500f, 7800f };
 
-        
-        Car car1 = new Car("Mercedes", "CLK", 2006, 2);
-        Car car2 = (Car)car1.Clone(); 
-
-        Plane plane1 = new Plane(35, "Boeing", "747", 2018);
-        Plane plane2 = (Plane)plane1.Clone(); 
-
-        garage.AddVehicle(car1);
-        garage.AddVehicle(car2);
-        garage.AddVehicle(plane1);
-        garage.AddVehicle(plane2);
-
-        foreach (var vehicle in garage)
+        var actors = new[]
         {
-            Console.WriteLine(vehicle);
-            vehicle.StartTheEngine();
+            new Actor {firstName = "Steven", lastName = "Seagal"},
+            new Actor {firstName = "Tom", lastName = "Cruise"},
+            new Actor {firstName = "Johny", lastName = "Depp"},
+        };
+
+        IEnumerable<string> mostFamous = actors.Where(Actor => Actor.firstName == "Johny")
+                                               .Select(Actor => $"{Actor.firstName} {Actor.lastName}");
+
+
+
+
+        var movies = new[]
+        {
+            new Movie {title = "China Salesman", starring = "Steven Seagal", profits = 175000000 },
+            new Movie {title = "Mission Impossible", starring = "Tom Crise", profits = 238000000},
+            new Movie {title = "Pirates of the Caribean", starring = "Johny Depp", profits = 342000000},
+        };
+
+        IEnumerable<string> mostProfitable = movies.OrderByDescending(movie => movie.profits)
+                                                   .Select(movie => $"{movie.title} - {movie.profits}");
+        List<int> evenNumbers = Filter(numbers, IsEven);
+        List<int> oddNumbers = Filter(numbers, IsOdd);
+
+        List<int> repeatedNumbers = FilterRepeated(numbers, delegate(int num, List<int> nums)
+        {
+            int count = 0;
+            foreach (int n in nums)
+            {
+                if (n == num) count++;
+                if (count > 1) return true;
+            }
+            return false;
+        });
+
+        List<float> numberOfDDS = FilterDDS(sums, sum => ((sum / 1.2f) - sum) * -1);
+        List<float> netSum = sums.extractionDDS();
+
+        Console.WriteLine("Repeated numbers:");
+        repeatedNumbers.ForEach(n => Console.WriteLine(n));
+
+
+        Console.WriteLine("Even numbers:");
+        evenNumbers.ForEach(n => Console.WriteLine(n));
+
+        Console.WriteLine("\nOdd numbers:");
+        oddNumbers.ForEach(n => Console.WriteLine(n));
+
+        Console.WriteLine("\nSumm DDS:");
+        numberOfDDS.ForEach(sum => Console.WriteLine(sum));
+
+        Console.WriteLine("\nSum without DDS");
+        netSum.ForEach(sum => Console.WriteLine(sum));
+
+       
+        foreach(var actor in mostFamous)
+        {
+            Console.WriteLine("\nMost famous actor: " + actor);
         }
+        foreach(var movie in mostProfitable)
+        {
+            Console.WriteLine("\nMost porfitable movies: " + movie + "$");
+        }
+        
+    }
+
+    static List<int> Filter(List<int> numbers, Condition condition)
+    {
+        List<int> result = new List<int>();
+        foreach (int number in numbers)
+        {
+            if (condition(number))
+            {
+                result.Add(number);
+            }
+            
+        }
+        return result;
+    }
+
+
+    static List<int> FilterRepeated(List<int> numbers1, ConditionCount condition1) 
+    {
+        List<int> result = new List<int> ();
+        foreach (int number1 in numbers1)
+        {
+            if (condition1(number1, numbers1) && !result.Contains(number1))
+
+            {
+                result.Add(number1);
+            }
+        }
+        return result;
+    }
+
+    static List<float> FilterDDS(List<float> sums, CalculateDDS calculation)
+    {
+        List<float> result = new List<float>();
+        foreach (float sum in sums)
+        {
+            result.Add(calculation(sum));
+        }
+        return result;
+    }
+
+    static bool IsEven(int number) => number % 2 == 0;
+    static bool IsOdd(int number) => number % 2 != 0;
+}
+
+public static class ListFloatExtensions
+{
+    public static List<float> extractionDDS(this List<float> sums)
+    {
+        List<float> result = new List<float>();
+        foreach (float sum in sums)
+        {
+            float dds = sum - (sum / 5f);
+            result.Add(dds);
+        }
+        return result;
     }
 }
